249d38884f4f539f7305fcfa33ddd886b8cb0031
scuxxs <93957347+scuxxs@users.noreply.github.com>
2024-03-07 16:40:34 +0800

    处理数据

--- process.py
+++ process.py
@@ -13,3 +13,23 @@ spark = SparkSession \
 
 pandas_df1 = pd.read_excel('./dataset/青年大学习.xlsx')
 spark_df1 = spark.createDataFrame(pandas_df1)
+
+schema = StructType([
+    StructField("id", StringType(), True),
+    StructField("name", StringType(), True),
+    StructField("college", StringType(), True),
+    StructField("major", StringType(), True),
+    StructField("node_name", StringType(), True),
+    StructField("late_level", IntegerType(), True),
+    StructField("cheat_level", IntegerType(), True),
+    StructField("poverty_level", IntegerType(), True),
+    StructField("politics_level", IntegerType(), True),
+    StructField("academy_level", IntegerType(), True),
+    StructField("mental_level", IntegerType(), True),
+    StructField("total_grade", IntegerType(), True)
+])
+
+# 创建DataFrame
+student_df = spark.createDataFrame([], schema)
+
+student_df.printSchema()
